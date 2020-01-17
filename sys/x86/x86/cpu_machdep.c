527698efdea09c5fc8fb331c75e3fa6a0c71c33d
ckyzee <ckyzee@freebsdfoundation.org>
2020-01-17 11:52:11 -0500

    Fixed sysctl mitigation senses

--- sys/x86/x86/cpu_machdep.c
+++ sys/x86/x86/cpu_machdep.c
@@ -985,6 +985,10 @@ hw_ssb_recalculate(bool all_cpus)
 	case 1: /* on */
 		hw_ssb_set(true, all_cpus);
 		break;
+	case 2: /* auto */
+		hw_ssb_set((cpu_ia32_arch_caps & IA32_ARCH_CAP_SSB_NO) != 0 ?
+		    false : true, all_cpus);
+		break;
 	}
 }
 
